3063051160abfb0a776be2664076f685f9508c32
Joshua King <windosbk+github@gmail.com>
2015-11-27 13:42:10 +1300

    Getting rid of job idea
    Because it closes the client when it finishes.

--- PowerBot/Public/Receive-XmppPresence.ps1
+++ PowerBot/Public/Receive-XmppPresence.ps1
@@ -18,30 +18,13 @@
     )
 
     if ($Presence.From.User -ne 'PowerBot' -and $Presence.From.Resource -notlike '*powerbot*')
-    {
-        $JobName = "Greet-$($Presence.from.User)"
-        $RelevantJob = Get-Job -Name $JobName -ErrorAction SilentlyContinue
-    
+    {    
         if ($Presence.Type -eq 'available')
         {
-            if ($RelevantJob -eq $null)
-            {
-                Start-Job -Name $JobName -ScriptBlock {
-                    Start-Sleep -Seconds 60
-                }
-            }
+            Write-ViewerGreeting -User $Presence.from.User
         }
         elseif ($Presence.Type -eq 'unavailable')
         {
-            foreach ($Job in $RelevantJob) 
-            {
-                if ($Job.State -ne 'Completed')
-                {
-                    $RelevantJob | Stop-Job
-                    Start-Sleep -Seconds 1
-                    $RelevantJob | Remove-Job    
-                }
-            }
         }
     }
 }
