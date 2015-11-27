fc28285d7121434960b089cce2ba069eb38e93f4
Joshua King <windosbk+github@gmail.com>
2015-11-27 15:45:11 +1300

    Viewer Class init

---
+++ PowerBot/Classes/ViewerClass.ps1
@@ -0,0 +1,17 @@
+﻿class Viewer
+{
+    [string] $Username
+    [int] $Points
+    [datetime[]] $Greeted
+    
+    Viewer ([string] $Username)
+    {
+        $this.Username = $Username
+    }
+
+    [void] Greet ()
+    {
+        $this.Greeted += Get-Date
+        Write-ViewerGreeting -User $this.Username
+    }
+}
