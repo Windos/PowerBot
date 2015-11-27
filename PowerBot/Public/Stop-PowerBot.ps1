57b9cc06bd1d07f1df999a98d21faaecca807985
Joshua King <windosbk+github@gmail.com>
2015-11-27 13:56:52 +1300

    Really need to stop stopping bot manually

---
+++ PowerBot/Public/Stop-PowerBot.ps1
@@ -0,0 +1,22 @@
+﻿function Stop-PowerBot
+{
+    <#
+        .SYNOPSIS
+        Short Description
+        .DESCRIPTION
+        Detailed Description
+        .EXAMPLE
+        Initialize-PowerBot
+        explains how to use the command
+        can be multiple lines
+        .EXAMPLE
+        Initialize-PowerBot
+        another example
+        can have as many examples as you like
+    #>
+    [CmdletBinding()]
+    Param ()
+
+    $Script:mucManager.LeaveRoom($Script:Room, $Script:Config.Username)
+    $Script:Client.Close()
+}
