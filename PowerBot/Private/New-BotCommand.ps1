2325373bdbc6fbfa946f12b7ffb56d05494bd092
Joshua King <windosbk+github@gmail.com>
2015-11-30 17:29:43 +1300

    New-BotCommand wrapper for class

---
+++ PowerBot/Private/New-BotCommand.ps1
@@ -0,0 +1,34 @@
+﻿function New-BotCommand
+{
+    <#
+        .Synopsis
+        Short description
+        .DESCRIPTION
+        Long description
+        .EXAMPLE
+        Example of how to use this cmdlet
+        .EXAMPLE
+        Another example of how to use this cmdlet
+    #>
+    [CmdletBinding()]
+    Param (
+        # Param1 help description
+        [Parameter(Mandatory = $true,
+                   Position = 0)]
+        [string] $Name,
+        
+        [string] $Command,
+        
+        [string] $Keyword,
+        
+        [int] $Cost,
+        
+        [string[]] $Group,
+        
+        [string] $Message,
+        
+        [switch] $Default
+    )
+    
+    $cmd = [BotCommand]::new($Name)
+}
