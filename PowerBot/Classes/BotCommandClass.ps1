6108878edf24cdf08555880ed4cef561261159f1
Joshua King <windosbk+github@gmail.com>
2015-11-30 17:20:33 +1300

    All about that method

--- PowerBot/Classes/BotCommandClass.ps1
+++ PowerBot/Classes/BotCommandClass.ps1
@@ -12,4 +12,39 @@
     {
         $this.Name = $Name
     }
+
+    [void] SetCommand ([string] $Command)
+    {
+        $this.Command = $Command
+    }
+
+    [void] SetKeyword ([string] $Keyword)
+    {
+        $this.Keyword = $Keyword
+    }
+
+    [void] SetMessage ([string] $Message)
+    {
+        $this.Message = $Message
+    }
+
+    [void] SetCost ([int] $Cost)
+    {
+        $this.Cost = $Cost
+    }
+    
+    [void] SetDefault ()
+    {
+        $this.Default = $true
+    }
+
+    [void] AddGroup ([string[]] $Group)
+    {
+        $this.Group += $Group
+    }
+
+    [void] RemoveGroup ([string[]] $Group)
+    {
+        $this.Group.Remove($Group)
+    }
 }
